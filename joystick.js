78d9dd8c61e43e239052cf08d89ec6f8c9a4fc7d
mathew <mathewdeanv@gmail.com>
2023-02-05 15:58:27 -0700

    timeout for 2ndary touch text

--- joystick.js
+++ joystick.js
@@ -33,7 +33,8 @@ export class Joystick{
                 this.joystickCenter.y = e.touches[i].clientY;
             }else{
                 this.notjoyTouches.push(e.touches[i].identifier);
-                //this.elem.innerHTML = e.touches[i].identifier;
+                this.elem.innerHTML = "ping";
+                setTimeout(() => {this.elem.innerHTML = "";}, 1000);
             }
         }
     }
